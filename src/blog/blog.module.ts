c0068cdbed708066449938d3934852dfa71a5765
hyc <992273994@qq.com>
2019-12-18 16:35:41 +0800

    'blog-config'

--- src/blog/blog.module.ts
+++ src/blog/blog.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { BlogController } from './blog.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { BlogEntity } from './blog.entity';
+import { BlogRepository } from './blog.repository';
 
 @Module({
-  imports:[TypeOrmModule.forFeature([BlogEntity])],
+  imports:[TypeOrmModule.forFeature([BlogRepository])],
   controllers: [BlogController]
 })
 export class BlogModule {}
