0e487501ad992913d6573b38926b38bd39697f69
hyc <992273994@qq.com>
2019-12-18 16:39:35 +0800

    'go'

--- src/blog/blog.module.ts
+++ src/blog/blog.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { BlogController } from './blog.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { BlogEntity } from './blog.entity';
 import { BlogRepository } from './blog.repository';
 
 @Module({
